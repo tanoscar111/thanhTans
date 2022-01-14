3525424db5d94623244862f4857c76623ada380c
tanoscar111 <tanoscar2810@gmail.com>
2022-01-14 22:13:28 +0700

    asdasd

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import Link from 'components/Link';
+import './index.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <span className="footer__date">{`© ${new Date().getFullYear()} Thanh Tan.`}</span>
+    <Link secondary className="footer__link" href="/humans.txt" target="_self">
+      Crafted by yours truly
+    </Link>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,38 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/',
+    hash: '#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/',
+    hash: '#details',
+  },
+  // {
+  //   label: 'Articles',
+  //   pathname: '/articles',
+  // },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/tanoscar2810',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.facebook.com/Eiziboiz',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/tanoscar111',
+    icon: 'github',
+  },
+];
